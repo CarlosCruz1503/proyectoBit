1eaeec2d59cb4335cb18185cafa31294b91463df
fredymar <fredymartinezquinto@hotmail.com>
2023-03-22 18:24:48 -0500

    Plantilla base

---
+++ js/main.js
@@ -0,0 +1,51 @@
+const input = document.querySelector("input");
+const addBtn = document.querySelector(".btn-add");
+const ul = document.querySelector(".ul-todo");
+const empty = document.querySelector(".empty");
+const error = document.querySelector(".vacio");
+
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const text = input.value;
+
+  if (text != "") {
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.textContent = text;
+
+    li.appendChild(p);
+    li.appendChild(addDeleteBtn());
+    ul.appendChild(li);
+
+    input.value = "";
+    empty.style.display = "none";
+  }else if (text == ""){
+    error.classList.remove("vacio");
+  }
+});
+
+input.addEventListener("focus", (e)=>{
+  error.classList.add("vacio");
+});
+
+
+function addDeleteBtn() {
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.textContent = "X";
+  deleteBtn.className = "btn-delete";
+
+  deleteBtn.addEventListener("click", (e) => {
+    const item = e.target.parentElement;
+    ul.removeChild(item);
+
+    const items = document.querySelectorAll("li");
+
+    if (items.length === 0) {
+      empty.style.display = "block";
+    }
+  });
+
+  return deleteBtn;
+}

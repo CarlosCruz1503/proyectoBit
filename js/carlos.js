c821be9b290ae22cf658046652afe173ea9fb2da
CarlosCruzBIT <bitcarloscruz@gmail.com>
2023-03-23 18:57:36 -0500

    pomodoro finish

--- js/carlos.js
+++ js/carlos.js
@@ -7,22 +7,30 @@ let seconds = 59
 let minutes = 24
 let bloqueo = 0
 
+var intervalId = null;
+
 workButtom.addEventListener("click", () => {
     seconds = 59
     minutes = 24
     time.textContent = `${minutes} : ${seconds}`
+    clearInterval(intervalId);
+    bloqueo = 0
+    startButtom.innerHTML = "START"
 })
 
 restButtom.addEventListener("click", () => {
     seconds = 59
     minutes = 4
     time.textContent = `${minutes} : ${seconds}`
+    clearInterval(intervalId);
+    bloqueo = 0
+    startButtom.innerHTML = "START"
 })
 
 startButtom.addEventListener("click", () => {
     bloqueo++
     if (bloqueo == 1) {
-        const interval = setInterval(() => {
+        intervalId = setInterval(() => {
             seconds--
             if (seconds == 00 && minutes != -1) {
                 seconds = 60
@@ -30,9 +38,15 @@ startButtom.addEventListener("click", () => {
             }
             time.textContent = `${minutes} : ${seconds}`;
             if (minutes == -1) {
-                clearInterval(interval)
+                clearInterval(intervalId)
                 time.textContent = "End";
             }
+            startButtom.innerHTML = "STOP"
         }, 1000)
+    }
+    if (bloqueo != 1) {
+        clearInterval(intervalId)
+        startButtom.innerHTML = "START"
+        bloqueo = 0
     }
 })

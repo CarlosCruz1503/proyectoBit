cedcb094fe4f40c05557732cccb66afb8f4d5a04
DanGuevara07 <guevaramdan@gmail.com>
2023-03-26 15:46:52 -0500

    iconos añadidos, funciones terminadas

--- js/roadMapTareas.js
+++ js/roadMapTareas.js
@@ -24,7 +24,7 @@ class taskData {
 // La variable Taskorig, guarda el estado inicial de la tabla para reiniciar los filtros.
 var myTasks = [];
 actualizarTabla();
-const myTasksorig = myTasks;
+var myTasksorig = myTasks;
 
 // Función que captura los elementos actuales de la tabla y los guarda en en arreglo de objetos myTasks
 function actualizarTabla(){
@@ -57,22 +57,36 @@ function imprimirTabla(){
     }
   }
 }
-
+// Funcion para realizar sorts
+function crearSort(elementid, asc, property, indicador){
+  document.getElementById(elementid).addEventListener('click', function(){
+    actualizarTabla();
+    if(asc){
+      myTasks.sort((a, b) => (a[property] < b[property]) ? 1 : -1);
+    }else{
+      myTasks.sort((a, b) => (a[property] > b[property]) ? 1 : -1);
+    }
+    imprimirTabla();
+    document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">'+indicador+'</button>'; 
+  });
+}
 
 // Función que espera al click de sort.tarea y reordena la vista
-document.getElementById("tareades").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.name > b.name) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Tarea Desc</button>'; 
-});
+crearSort("tareades", false, "name", "Tarea Desc");
+crearSort("tareaasc", true, "name", "Tarea Asc");
+// Función que espera al click de sort.Tipo y reordena la vista
+crearSort("tipodes", false, "typeOfTask", "Tipo Desc");
+crearSort("tipoasc", true, "typeOfTask", "Tipo Asc");
+// Función que espera al click de sort.Fechadeinicio y reordena la vista
+crearSort("fechaindes",false,"startDate", "Fecha de Inicio Desc");
+crearSort("fechainasc",true,"startDate", "Fecha de Inicio Asc");
+// Función que espera al click de sort.Fechadeentrega y reordena la vista
+crearSort("fechafides",false,"endDate", "Fecha de Entrega Desc");
+crearSort("fechafiasc",true,"endDate", "Fecha de Entrega Asc");
+// Función que espera al click de sort.Encargado y reordena la vista
+crearSort("encardes",false,"inChargeOf", "Encargado Desc");
+crearSort("encarasc",true,"inChargeOf", "Encargado Asc");
 
-document.getElementById("tareaasc").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.name < b.name) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Tarea Asc</button>'; 
-});
 
 // Función que espera al click de sort.estado y reordena la vista WIP porque no se como hacerlo
 // document.getElementById("estadodes").addEventListener('click', function(){
@@ -89,67 +103,6 @@ document.getElementById("tareaasc").addEventListener('click', function(){
 //   document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Tarea Asc</button>'; 
 // });
 
-// Función que espera al click de sort.Tipo y reordena la vista
-document.getElementById("tipodes").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.typeOfTask > b.typeOfTask) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Tipo Desc</button>'; 
-});
-
-document.getElementById("tipoasc").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.typeOfTask < b.typeOfTask) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Tipo Asc</button>'; 
-});
-
-// Función que espera al click de sort.Fechadeinicio y reordena la vista
-document.getElementById("fechaindes").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.startDate > b.startDate) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Fecha de Inicio Desc</button>'; 
-});
-
-document.getElementById("fechainasc").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.startDate < b.startDate) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Fecha de Inicio Asc</button>'; 
-});
-
-// Función que espera al click de sort.Fechadeentrega y reordena la vista
-document.getElementById("fechafides").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.endDate > b.endDate) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Fecha de Entrega Desc</button>'; 
-});
-
-document.getElementById("fechafiasc").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.endDate < b.endDate) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Fecha de Entrega Asc</button>'; 
-});
-
-// Función que espera al click de sort.Encargado y reordena la vista
-document.getElementById("encardes").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.inChargeOf > b.inChargeOf) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Encargado Desc</button>'; 
-});
-
-document.getElementById("encarasc").addEventListener('click', function(){
-  actualizarTabla();
-  myTasks.sort((a, b) => (a.inChargeOf < b.inChargeOf) ? 1 : -1);
-  imprimirTabla();
-  document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-primary ms-5">Encargado Asc</button>'; 
-});
-
-
 
 
 // funcion para deshacer y retornar a la vista original de la tabla
@@ -160,10 +113,44 @@ document.getElementById("deshacer").addEventListener('click', function(){
   document.getElementById("indicadorFiltro").innerHTML =  '<button class="btn btn-dark ms-5">Sin Filtro</button>'; 
 });
 
-document.getElementById("completado").addEventListener('click', function(){
-  document.getElementById("inprogress").addEventListener('click', function(){
-    document.getElementById("completado").innerHTML =  '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">En Progreso</button>';
+//Funcion para trigger modal y cambiar botones estado
+
+function cambiarEstado(opcion, fila){
+  let element = document.getElementById(opcion);
+  let newElement = element.cloneNode(true);
+  element.replaceWith(newElement);
+  document.getElementById(opcion).addEventListener('click', function(){
+    if(opcion==="opt1"){
+      document.getElementById(fila).innerHTML =  '<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#exampleModal">Completado</button>';
+    }
+    if(opcion==="opt2"){
+      document.getElementById(fila).innerHTML =  '<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">En Progreso</button>';
+    }
+    if(opcion==="opt3"){
+      document.getElementById(fila).innerHTML =  '<button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#exampleModal">Con Demora</button>';
+    }
+    if(opcion==="opt4"){
+      document.getElementById(fila).innerHTML =  '<button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#exampleModal">Aplazado</button>';
+    }
+    if(opcion==="opt5"){
+      document.getElementById(fila).innerHTML =  '<button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">Cancelado</button>';
+    }
+    if(opcion==="opt6"){
+      document.getElementById(fila).innerHTML =  '<button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">No Iniciado</button>';
+    }
     actualizarTabla();
+    myTasksorig = myTasks;
     imprimirTabla();
   });
-});
+}
+
+for(let i=1; i<myTasks.length+1; i++){
+  document.getElementById("fila"+i).addEventListener('click', function(){
+    cambiarEstado("opt1", "fila"+i);
+    cambiarEstado("opt2", "fila"+i);
+    cambiarEstado("opt3", "fila"+i);
+    cambiarEstado("opt4", "fila"+i);
+    cambiarEstado("opt5", "fila"+i);
+    cambiarEstado("opt6", "fila"+i);
+  });
+}
